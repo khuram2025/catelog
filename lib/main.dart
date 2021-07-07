b0edd1c12e40efe07524c8ce5b661c250678529e
Khuram Khan <khuram2025@gmail.com>
2021-07-07 16:18:59 +0300

    Day 10 Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catelog/pages/home_page.dart';
 import 'package:catelog/pages/login.dart';
 import 'package:catelog/utils/routes.dart';
+import 'package:catelog/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: MyTheme.lightTheme(context),
       // home: LoginPage(),
       routes: {
         '/': (context) => HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Catelog"
+          "Catelog App"
         ),
       ),
       body: Column(
